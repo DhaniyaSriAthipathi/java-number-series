7f294e0607955a82bfcaf624e7b3a43c09c008a9
DhaniyaSriAthipathi <athipathidhaniya@gmail.com>
2026-07-01 22:30:09 +0530

    Add files via upload

---
+++ Differenceincreaseby1.java
@@ -0,0 +1,17 @@
+import java.util.*;
+public class Differenceincreaseby1 {
+    public static void main(String[] args) {
+        Scanner sc=new Scanner(System.in);
+        int n=sc.nextInt();
+        int a=0;
+        int b=1;
+        int c;
+        for(int i=0;i<=n;i++)
+        {
+            c=a+b;
+            a=b;
+            b=c;
+            System.out.println(c+1);
+        }
+    }
+}
