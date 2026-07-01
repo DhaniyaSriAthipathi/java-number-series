6484e79697f0a0c10c5911543063376ca5a572df
DhaniyaSriAthipathi <athipathidhaniya@gmail.com>
2026-07-01 22:31:14 +0530

    Add files via upload

---
+++ Printno.java
@@ -0,0 +1,10 @@
+public class Printno {
+    public static void main(String[] args) {
+        //print numbers from 1 to 100
+        //start range 1 upto 100
+        for(int i=1;i<=100;i++)
+        {
+            System.out.println(i);
+        }
+    }
+}
