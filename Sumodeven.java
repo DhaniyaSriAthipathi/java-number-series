3ed4164d9a943d13b0679af25afd54082a036529
DhaniyaSriAthipathi <athipathidhaniya@gmail.com>
2026-07-04 22:17:19 +0530

    Add files via upload

---
+++ Sumodeven.java
@@ -0,0 +1,18 @@
+import java.util.Scanner;
+
+public class Sumodeven {
+    public static void main(String[] args) {
+        Scanner sc=new Scanner(System.in);
+        int n=sc.nextInt();
+        int sum=0;
+        // sum of even numbers
+        for(int i=0;i<=n;i++){
+            if(i%2==0)
+            {
+                sum+=i;
+            }
+        }
+        System.out.println(sum);
+        sc.close();
+    }
+}
